2c35d052dffe5bd6c26bff8e8b26dc6066404a96
gusttavori <gusta.contato@outlook.com>
2026-02-12 15:58:36 -0300

    Fix academic icon

--- vibz_front/src/app/page.js
+++ vibz_front/src/app/page.js
@@ -291,7 +291,7 @@ export default function Home() {
     // Configuração visual das categorias
     const categoriesConfig = [
         // MUDANÇA DE ÍCONE AQUI (Certifique-se de ter a imagem 'academic.svg' ou similar na pasta public/img)
-        { name: 'Acadêmico', icon: '/img/academic.svg', ref: academicoRef, key: 'academico' }, 
+        { name: 'Acadêmico', icon: '/img/academic.png', ref: academicoRef, key: 'academico' }, 
         { name: 'Festas e Shows', icon: '/img/music.svg', ref: festasRef, key: 'festas' },
         { name: 'Teatro', icon: '/img/theater.svg', ref: teatroRef, key: 'teatro' },
         { name: 'Esportes', icon: '/img/sports.svg', ref: esportesRef, key: 'esportes' },
